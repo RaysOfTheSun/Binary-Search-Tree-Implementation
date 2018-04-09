78fb856e4a7787eb2b746bf27342cd3ba62f6c6c
RaysOfTheSun <rdelsol35@gmail.com>
2018-04-09 18:49:49 +0800

    Update BinarySearchTree class
    - Added methods for traversing the tree in postorder, preorder and inorder
    - Fixed bug in where duplicate items in the tree do not get deleted

--- DataStructures/Main.py
+++ DataStructures/Main.py
@@ -2,7 +2,20 @@ from Trees.BinaryTree.BinarySearchTree import BinarySearchTree
 
 tree = BinarySearchTree()
 
-items = [10, 50, 40, 50, 60, 30, 45, 55, 70, 51, 56, 61, 80, 5, 4, 6]
-
+items = ['q', 'w', 'e', 't', 'y', 'u', 'i', 'o', 'p']
+# 10, 50, 40, 50, 60, 30, 45, 55, 70, 51, 56, 61, 80, 5, 4, 6
 for item in items:
-    tree.insert_item(item)
+    tree.add(item)
+
+tree.show_traversals()
+tree.remove('a')
+print()
+tree.show_traversals()
+
+
+value = (input("Enter a value to find in the tree: "))
+
+if tree.find(value) is True:
+    print("The value {} is in the tree.".format(value))
+else:
+    print("The value {} is not in the tree.".format(value))
--- DataStructures/Trees/BinaryTree/BinarySearchTree.py
+++ DataStructures/Trees/BinaryTree/BinarySearchTree.py
@@ -10,45 +10,43 @@ class BinarySearchTree:
 
     __RootNode = None
 
-    """Methods that makes use of the [operation_item] naming template is a method 
-    that acts as an interface to the actual method [operation]. This is because 
-    we do not want to reference the tree's root node in the client code.
-    Another thing is, I can't use default arguments as there are situations in where
-    a node has to be checked if it is null (if node is None)"""
-
     def __init__(self):
         self.__RootNode = None
 
     # Inserts an item into the binary search tree
-    # Returns an item of type Node
-    def __insert(self, node, data)->Node:
+    def __add(self, node, data)->Node:
         if node is None:  # Null is equivalent to None in python
             node = Node(data)
         elif data <= node.data:
-            node.left_child = self.__insert(node.left_child, data)
+            node.left_child = self.__add(node.left_child, data)
         else:
-            node.right_child = self.__insert(node.right_child, data)
+            node.right_child = self.__add(node.right_child, data)
         return node
 
-    # This method serves as an interface to the 'insert' method.
-    # I think it is quite annoying to repeatedly reference the root node
-    # for every insertion operation
-    def insert_item(self, data):
-        self.__RootNode = self.__insert(self.__RootNode, data)
+    # Serves as an interface to the '__add' method.
+    # This avoids repeatedly referencing the root node
+    # outside of this class
+    def add(self, data):
+        self.__RootNode = self.__add(self.__RootNode, data)
 
-    def __search(self, node, key)->bool:
+    # Searches for an item in the tree
+    def __find(self, node, key)->bool:
         if node is None:  # Since we ran out of nodes to search,
             return False  # the item is not in the tree
         elif key < node.data:
-            return self.__search(node.left_child, key)
+            return self.__find(node.left_child, key)
         elif key > node.data:
-            return self.__search(node.right_child, key)
+            return self.__find(node.right_child, key)
         else:
             return True
 
-    def search_item(self, key)->bool:
-        return self.__search(self.__RootNode, key)
+    # Serves as an interface to the '__find' method.
+    # This avoids repeatedly referencing the root node
+    # outside of this class
+    def find(self, key)->bool:
+        return self.__find(self.__RootNode, key)
 
+    # Returns the node's existing child
     @staticmethod
     def __get_child(node)->Node:
         if node.right_child is not None:
@@ -56,6 +54,7 @@ class BinarySearchTree:
         else:
             return node.left_child
 
+    # Finds the minimum value in the given subtree
     def __find_min(self, node)->int:
         # Go as deep as possible in the node's left
         # subtree as it will contain values that are
@@ -66,11 +65,11 @@ class BinarySearchTree:
             return node.data
 
     # Removes an item from the tree
-    def __delete(self, node, key)->Node:
+    def __remove(self, node, key)->Node:
         if key < node.data:
-            node.left_child = self.__delete(node.left_child, key)
+            node.left_child = self.__remove(node.left_child, key)
         elif key > node.data:
-            node.right_child = self.__delete(node.right_child, key)
+            node.right_child = self.__remove(node.right_child, key)
         else:  # we found the item in the tree. Let's get rid of it
             # The item to be deleted is a leaf node
             if (node.right_child is None) and (node.left_child is None):
@@ -82,8 +81,67 @@ class BinarySearchTree:
             # The item to be deleted is a node that has two children
             elif (node.right_child is not None) and (node.left_child is not None):
                 node.data = self.__find_min(node.right_child)
-                node.right_child = self.__delete(node.right_child, node.data)
+                node.right_child = self.__remove(node.right_child, node.data)
         return node
 
-    def delete_item(self, key):
-        self.__RootNode = self.__delete(self.__RootNode, key)
+    # Serves as an interface to the '__remove' method.
+    # This avoids repeatedly referencing the root node
+    # outside of this class
+    def remove(self, key):
+        # As the item is not in the tree, there is nothing to delete
+        if(self.find(key)) is False:
+            return
+        else:
+            self.__RootNode = self.__remove(self.__RootNode, key)
+            if (self.find(key)) is True:
+                # Check for duplicates and get rid of them too
+                self.__RootNode = self.__remove(self.__RootNode, key)
+            else:
+                return
+
+    def __traverse_inorder(self, node):
+        if node is not None:
+            self.__traverse_inorder(node.left_child)
+            print(node.data, end=', ')
+            self.__traverse_inorder(node.right_child)
+
+    # Serves as an interface to the '__traverse_inorder' method.
+    # This eliminates the need to reference the root node
+    # outside of this class
+    def traverse_inorder(self):
+        self.__traverse_inorder(self.__RootNode)
+
+    # Serves as an interface to the '__traverse_postorder' method.
+    # This eliminates the need to reference the root node
+    # outside of this class
+    def __traverse_postorder(self, node):
+        if node is not None:
+            self.__traverse_postorder(node.left_child)
+            self.__traverse_postorder(node.right_child)
+            print(node.data, end=', ')
+
+    def traverse_postorder(self):
+        self.__traverse_postorder(self.__RootNode)
+
+    def __traverse_preorder(self, node):
+        if node is not None:
+            print(node.data, end=', ')
+            self.__traverse_preorder(node.left_child)
+            self.__traverse_preorder(node.right_child)
+
+    # Serves as an interface to the '__traverse_preorder' method.
+    # This eliminates the need to reference the root node
+    # outside of this class
+    def traverse_preorder(self):
+        self.__traverse_preorder(self.__RootNode)
+
+    def show_traversals(self):
+        print("Preorder Traversal: ", end=' ')
+        self.traverse_preorder()
+        print()
+        print("Postorder Traversal: ", end=' ')
+        self.traverse_postorder()
+        print()
+        print("Inorder Traversal: ", end=' ')
+        self.traverse_inorder()
+        print()
